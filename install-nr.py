7917c1acba1de6668629f11c1ca59e8e1edf4390
Quentin Nambot <quentin.nambot@bedrockstreaming.com>
2022-05-06 15:37:04 +0200

    feat: update file

--- install-nr.py
+++ install-nr.py
@@ -1,25 +1,25 @@
 # Databricks notebook source
 # Initialization script to generate installer for nri-databricks
 
-dbutils.fs.put("dbfs:/newrelic/nri-databricks-init.sh","""
+dbutils.fs.put("dbfs:/newrelic/nri-databricks-init.sh",""" 
 #!/bin/sh
 
 install_nri_databricks () {
 
 if [ $DB_IS_DRIVER ]; then
     # Root user detection
-    if [ $(echo " $UID ") = " 0 " ];
-    then
-        sudo=''
+    if [ $(echo " $UID ") = " 0 " ];                                      
+    then                                                                     
+        sudo=''                                                                
     else
-        sudo='sudo'
+        sudo='sudo'                                                        
     fi
 
     echo " >>> Check if this is driver ? $DB_IS_DRIVER "
     echo " >>> Spark Driver ip : $DB_DRIVER_IP "
     echo " >>> Public DNS: $CONF_PUBLIC_DNS "
     echo " >>> UI Port: $CONF_UI_PORT "
-
+    
     # Download or copy nri-databricks binary
     wget https://github.com/newrelic-experimental/nri-databricks/releases/download/v1.0.1/nri-databricks.tar.gz -P /tmp
     cd /etc
@@ -28,7 +28,7 @@ if [ $DB_IS_DRIVER ]; then
     # fetch requirements
     cd /etc/nri-databricks
     python -m pip install -r requirements.txt || true
-
+    
     if [ -e "/tmp/driver-env.sh" ]; then
         source /tmp/driver-env.sh
         echo "CONF_PUBLIC_DNS is $CONF_PUBLIC_DNS"
@@ -37,19 +37,20 @@ if [ $DB_IS_DRIVER ]; then
         CONF_PUBLIC_DNS='<<CONF_PUBLIC_DNS>>'
         CONF_UI_PORT='<<CONF_UI_PORT>>'
     fi
-
+    
     if [ -e "/tmp/master-params" ]; then
         MASTER_UI_PORT=$(cat /tmp/master-params | cut -d ' ' -f 2)
     else
         MASTER_UI_PORT='<<MASTER_UI_PORT>>'
     fi
-
-    if [ $NEWRELIC_API_ENDPOINT == "" ]; then
-        NEWRELIC_API_ENDPOINT="US"
+    
+        
+    if [ $NEWRELIC_ENDPOINT_REGION == "" ]; then
+        NEWRELIC_ENDPOINT_REGION="US"
     fi
 
     # Create config.yml file (take care to format properly as is required for a yml file)
-
+    
     echo "
 integration_name: com.nrlabs.databricks
 run_as_service: True
@@ -63,7 +64,7 @@ spark:
   conf_ui_port: $CONF_UI_PORT
   driver_host: $CONF_PUBLIC_DNS
 newrelic:
-  api_endpoint: $NEWRELIC_API_ENDPOINT
+  api_endpoint: $NEWRELIC_ENDPOINT_REGION
   account_id: $NEWRELIC_ACCOUNT_ID
   api_key: $NEWRELIC_LICENSE_KEY
 labels:
